795270b66188aecca8df616cfd1e29ee2eff8c48
Tom Pinckney <thomaspinckney3@gmail.com>
2013-04-04 22:50:25 -0400

    checkpoint

---
+++ db48.py
@@ -0,0 +1,289 @@
+import os
+import mmap
+import struct
+import time
+
+_TABLE_NUM_REGIONS = 1024
+_TABLE_HEADER_SZ = 4096 + _TABLE_NUM_REGIONS
+_TABLE_MAGIC_OFF = 0
+_TABLE_MAGIC = 0xdb48beef
+_TABLE_CSUM_OFF = 4
+_TABLE_REGION_SUMMARY_OFF = 4096
+_TABLE_EPOCH = 1364768380
+
+_REGION_NUM_FMES = 1024
+_REGION_FME_SZ = 4
+_REGION_SZ = 64*1024
+_REGION_HEADER_SZ = _REGION_NUM_FMES * _REGION_FME_SZ
+_REGION_USABLE_SZ = _REGION_SZ - _REGION_HEADER_SZ
+
+_TABLE_SZ = _TABLE_HEADER_SZ + _REGION_SZ * _TABLE_NUM_REGIONS
+
+_FLS_MAGIC_OFF = 0
+_FLS_MAGIC = 0x0ff537
+_FLS_LEN_OFF = 4
+_FLS_NEXT_OFF = 6
+_FLS_HEADER_SZ = 8
+
+_FIELD_MAGIC_OFF = 0
+_FIELD_MAGIC = 0x48
+_FIELD_TYPE_OFF = 1
+_FIELD_KEY_OFF = 2
+_FIELD_TS_OFF = 4
+_FIELD_HEADER_SZ = 8
+
+FL_TYPE_INT = 1
+FL_TYPE_BYTES = 2
+
+class Table:
+
+    def __init__(self):
+        self.initialized = False
+
+    def create(self, path):
+        assert self.initialized == False
+        self._fd = os.open(path, os.O_RDWR | os.O_CREAT)
+        os.lseek(self._fd, _TABLE_SZ-1, os.SEEK_SET)
+        os.write(self._fd, '\x00')
+        self._mmap = mmap.mmap(self._fd, _TABLE_SZ)
+        self._mmap[_TABLE_MAGIC_OFF:_TABLE_MAGIC_OFF+4] = struct.pack(">I", _TABLE_MAGIC)
+        self._mmap[_TABLE_CSUM_OFF:_TABLE_CSUM_OFF+4] = struct.pack(">I", 0)
+        for i in range(_TABLE_NUM_REGIONS):
+            self._mmap[i+_TABLE_CSUM_OFF] = struct.pack("B", 0)
+        for i in range(_TABLE_NUM_REGIONS):
+            r = Region(self, i)
+            r.create()
+        self.initialized = True
+
+    def open(self, path):
+        assert self.initialized == False
+        self._fd = os.open(path, os.O_RDWR)
+        self._mmap = mmap.mmap(self._fd, _TABLE_SZ)
+        magic = struct.unpack(">I", self._mmap[_TABLE_MAGIC_OFF:_TABLE_MAGIC_OFF+4])[0]
+        assert magic == _TABLE_MAGIC
+        self.initialized = True
+
+    def insert(self, fls):
+        assert self.initialized
+        rec_addr = self._insert(fls)
+        return rec_addr
+
+    def _insert(self, fls):
+        space = fls.length()
+        region = self._find_region_with_space(space)
+        rec_addr = region.insert(fls, space)
+        return rec_addr
+
+    def update(self, rec_addr, fls):
+        assert self.initialized
+        existing_fls, region = self._lookup(rec_addr)
+        if existing_fls.length() < fls.length():
+            self._delete(rec_addr, region)
+            existing_fls.update(fls)
+            self._insert(existing_fls)
+        else:
+            existing_fls.update(fls)
+            region.update(existing_fls)
+
+    def delete(self, rec_addr):
+        assert self.initialized
+        self._delete(rec_addr)
+
+    def _delete(self, rec_addr, region=None):
+        if region is None:
+            region = self._find_region_with_rec(rec_addr)
+        region.delete(rec_addr)
+
+    def lookup(self, rec_addr):
+        assert self.initialized
+        fls, _ = self._lookup(rec_addr)
+        return fls
+
+    def _lookup(self, rec_addr):
+        region = self._find_region_with_rec(rec_addr)
+        fls = region.read(rec_addr)
+        return fls, region
+
+    def close(self):
+        assert self.initialized
+        self._mmap.close()
+        os.close(self._fd)
+        self.initialized = False
+
+    def _find_region_with_space(self, space):
+        region_summaries = struct.unpack_from(">" + "B"*_TABLE_NUM_REGIONS, self._mmap[_TABLE_REGION_SUMMARY_OFF:])
+        tries = 0
+        for ndx in range(_TABLE_NUM_REGIONS):
+            percent_full = float(region_summaries[ndx]) / 100.0            
+            free_space = _REGION_USABLE_SZ * (1.0 - percent_full)
+            if free_space >= space and percent_full <= 0.95 :
+                return Region(self, ndx)
+        raise NoSpace()
+
+    def _find_region_with_rec(self, rec_addr):
+        ndx = rec_addr / _REGION_USABLE_SZ
+        return Region(self, ndx)
+
+class Region:
+    
+    #
+    # Invariants:
+    #
+    # 1) There are always _REGION_NUM_FMES FME's (free-space-maps) to list extents of
+    # free space in the region.
+    #
+    # 2) "Empty" FMEs have their length == 0 and the empty FMEs always come after
+    # the full ones.
+    #
+    # 3) Full FMEs are sorted in order of increasing offset.
+    #
+
+    class FME():
+        def __init__(self, o, l):
+            self.offset = o
+            self.length = l
+
+    def __init__(self, table, ndx):
+        self.table = table
+        self.offset = _TABLE_HEADER_SZ+ndx*_REGION_SZ
+        self.ndx = ndx
+
+    def create(self):
+        fme = self.FME(0, _REGION_USABLE_SZ)
+        self.table._mmap[self.offset:self.offset+4] = struct.pack(">HH", fme.offset, fme.length)
+        # relies on rest of FME array already being zero'd which fills each fme.length with zero
+        # which is the sentinel meaning end-of-valid-fmes.
+
+    def _load_fmes(self):
+        raw_fmes = struct.unpack(">" + "HH"*_REGION_NUM_FMES, self.table._mmap[self.offset:self.offset+_REGION_HEADER_SZ])
+        assert len(raw_fmes) == 2 * _REGION_NUM_FMES
+        fmes = [self.FME(raw_fmes[2*i], raw_fmes[2*i+1]) for i in range(_REGION_NUM_FMES)]
+        return fmes
+
+    def _store_fmes(self, fmes):
+        assert len(fmes) == _REGION_NUM_FMES
+        raw_fmes = [struct.pack(">HH", fme.offset, fme.length) for fme in fmes]
+        self.table._mmap[self.offset:self.offset+_REGION_HEADER_SZ] = "".join(raw_fmes)
+
+        temp_fmes = self._load_fmes()
+        for i in range(len(temp_fmes)):
+            assert fmes[i].length == temp_fmes[i].length
+    
+    def insert(self, fls, space):
+        fmes = self._load_fmes()
+        assert fmes[1].offset == 0
+        assert fmes[1].length == 0
+        for i in range(_REGION_NUM_FMES):
+            if fmes[i].length == 0: break   # length == 0 means we're at end of valid fmes
+            if fmes[i].length < space: continue
+            rec_addr = fmes[i].offset + self.ndx * _REGION_USABLE_SZ
+            if fmes[i].length == space:
+                assert False
+                del fmes[i]
+                empty_fme = FME(0, 0)
+                fmes.append(empty_fme)
+            else:
+                fmes[i].offset += space
+                fmes[i].length -= space
+            assert fmes[1].offset == 0
+            assert fmes[1].length == 0
+            self._store_fmes(fmes)
+            rec_offset = rec_addr + (self.ndx + 1) * _REGION_HEADER_SZ + _TABLE_HEADER_SZ
+            fls.store(rec_offset, self.table._mmap)
+            return rec_addr
+        raise NoSpace()
+
+    def read(self, rec_addr):
+        rec_off_in_region = rec_addr % _REGION_USABLE_SZ
+        offset = self.offset + _REGION_HEADER_SZ + rec_off_in_region
+        fls = FieldList.load(offset, self.table._mmap)
+        return fls
+
+class FieldList:
+    def __init__(self, fls):
+        self.fls = fls
+
+    @staticmethod
+    def set(fls):
+        assert len(fls) > 0
+        fls = list(fls)
+        fls.sort(key=lambda x: x.key)
+        ts = int((time.time() - _TABLE_EPOCH) * 1000)
+        for fl in fls:
+            fl.ts = ts
+        return FieldList(fls)
+
+    def length(self):
+        return _FLS_HEADER_SZ + sum([fl.length() for fl in self.fls])
+
+    def store(self, offset, mmap):
+        raw_fls = "".join([fl.as_raw() for fl in self.fls])
+        raw_header = struct.pack(">IHH", _FLS_MAGIC, len(raw_fls) + _FLS_HEADER_SZ, 0)
+        assert len(raw_header) == _FLS_HEADER_SZ
+        raw = raw_header + raw_fls
+        assert self.length() == len(raw)
+        mmap[offset:offset+len(raw)] = raw
+
+    @staticmethod
+    def load(offset, mmap):
+        fls = []
+        rec_magic, rec_len, _ = struct.unpack(">IHH", mmap[offset:offset+8])
+        assert rec_magic == _FLS_MAGIC
+        offset += 8
+        rec_len -= 8
+        while rec_len > 0:
+            fl_len, fl = Field.from_raw(offset, mmap)
+            rec_len -= fl_len
+            offset += fl_len
+            fls.append(fl)
+        assert rec_len == 0
+        return FieldList(fls)
+
+class Field:
+    def __init__(self, type, key, value, ts=None):
+        self.type = type
+        self.key = key
+        self.value = value
+        self.ts = ts
+
+    def length(self):
+        length = _FIELD_HEADER_SZ
+        if self.type == FL_TYPE_INT:
+            length += 4
+        elif self.type == FL_TYPE_BYTES:
+            length += 2
+            length += len(self.value)
+        return length
+
+    def as_raw(self):
+        out = struct.pack(">BBHI", _FIELD_MAGIC, self.type, self.key, self.ts)
+        assert len(out) == _FIELD_HEADER_SZ
+        if self.type == FL_TYPE_INT:
+            out += struct.pack(">I", self.value)
+        elif self.type == FL_TYPE_BYTES:
+            out += struct.pack(">H", len(self.value))
+            out += self.value
+        assert self.length() == len(out)
+        return out
+
+    @staticmethod
+    def from_raw(offset, mmap):
+        length = _FIELD_HEADER_SZ
+        fl_magic, fl_type, fl_key, fl_ts = struct.unpack(">BBHI", mmap[offset:offset+_FIELD_HEADER_SZ])
+        assert fl_magic == _FIELD_MAGIC
+        assert fl_type in (FL_TYPE_INT, FL_TYPE_BYTES)
+        offset += _FIELD_HEADER_SZ
+        if fl_type == FL_TYPE_INT:
+            value = struct.unpack(">I", mmap[offset:offset+4])[0]
+            length += 4
+        elif fl_type == FL_TYPE_BYTES:
+            value_len = struct.unpack(">H", mmap[offset:offset+2])[0]
+            offset += 2
+            length += 2
+            value = mmap[offset:offset+value_len]
+            length += value_len
+        fl = Field(fl_type, fl_key, value, fl_ts)
+        return length, fl
+
+class NoSpace(Exception):
+    pass
---
+++ test.py
@@ -0,0 +1,119 @@
+import unittest
+import os
+import struct
+
+import db48
+
+class TestCreateClose(unittest.TestCase):
+    path = "/tmp/t.db48"
+
+    def setUp(self):
+        try:
+            os.unlink(self.path)
+        except OSError:
+            pass
+
+    def _create(self):
+        t = db48.Table()
+        t.create(self.path)
+        return t
+
+    def _open(self):
+        t = self._create()
+        t.close()
+        t = db48.Table()
+        t.open(self.path)
+        return t
+
+    def test_create_and_close(self):
+        t = self._create()
+        r = db48.Region(t, 0)
+        fmes = r._load_fmes()
+        self.assertTrue(fmes[0].offset == 0)
+        self.assertTrue(fmes[0].length == db48._REGION_USABLE_SZ)
+        self.assertTrue(fmes[1].offset == 0)
+        self.assertTrue(fmes[1].length == 0)
+        t.close()
+
+    def test_open(self):
+        t = self._open()
+        t.close()
+
+    def test_find_region_with_space1(self):
+        t = self._open()
+        region = t._find_region_with_space(78)
+        self.assertTrue(region.offset == db48._TABLE_HEADER_SZ)  # first region
+        self.assertRaises(db48.NoSpace, t._find_region_with_space, 64 * 1024 + 1) # too big
+
+    def test_insert(self):
+        t = self._open()
+        r = db48.Region(t, 0)
+        fmes = r._load_fmes()
+        self.assertTrue(fmes[0].offset == 0)
+        self.assertTrue(fmes[0].length == db48._REGION_USABLE_SZ)
+        self.assertTrue(fmes[1].offset == 0)
+        self.assertTrue(fmes[1].length == 0)
+        msg = "Hello, World!".encode("utf-8")
+        fl1 = db48.Field(db48.FL_TYPE_INT, 0, 42)
+        fl2 = db48.Field(db48.FL_TYPE_BYTES, 1, msg)
+        fls = db48.FieldList.set((fl1, fl2))
+        space = fls.length()
+        self.assertTrue(space == (db48._FIELD_HEADER_SZ*2 + db48._FLS_HEADER_SZ + 4 + 2 + len(msg)))
+        rid = t.insert(fls)
+        self.assertTrue(rid == 0)
+        r = db48.Region(t, 0)
+        fmes = r._load_fmes()
+        self.assertTrue(fmes[0].offset == space)
+        self.assertTrue(fmes[0].length == db48._REGION_USABLE_SZ - space)
+        self.assertTrue(fmes[1].length == 0)
+        t.close()
+
+    def test_lookup(self):
+        t = self._open()
+        msg = "Hello, World!".encode("utf-8")
+        fl1 = db48.Field(db48.FL_TYPE_INT, 0, 42)
+        fl2 = db48.Field(db48.FL_TYPE_BYTES, 13, msg)
+        fls = db48.FieldList.set((fl1, fl2))
+        rid = t.insert(fls)
+        fls = t.lookup(rid)
+        self.assertTrue(fls.fls[0].value == 42)
+        self.assertTrue(fls.fls[0].key == 0)
+        self.assertTrue(fls.fls[1].value == msg)
+        self.assertTrue(fls.fls[1].key == 13)        
+        t.close()
+
+    def test_insert2(self):
+        t = self._open()
+        total_space = 0
+        records = []
+        for i in range(1000):
+            fl1 = db48.Field(db48.FL_TYPE_INT, 0, i)
+            fl2 = db48.Field(db48.FL_TYPE_BYTES, 1, ("Hello %d" % i).encode("utf-8"))
+            fls = db48.FieldList.set((fl1, fl2))
+            space = fls.length()
+            rid = t.insert(fls)
+            self.assertTrue(rid == total_space)
+            total_space += space
+            records.append(rid)
+        r = db48.Region(t, 0)
+        fmes = r._load_fmes()
+        self.assertTrue(fmes[0].offset == total_space)
+        self.assertTrue(fmes[0].length == db48._REGION_USABLE_SZ - total_space)
+        self.assertTrue(fmes[1].length == 0)
+        offset = r.offset + db48._REGION_HEADER_SZ
+        for i in range(1000):
+            rec_magic, rec_len, _ = struct.unpack(">IHH", r.table._mmap[offset:offset+8])
+            assert rec_magic == db48._FLS_MAGIC
+            assert rec_len <= db48._FLS_HEADER_SZ + 2*db48._FIELD_HEADER_SZ + 4 + 2 + 9
+            assert rec_len >= db48._FLS_HEADER_SZ + 2*db48._FIELD_HEADER_SZ + 4 + 2 + 1
+            offset += rec_len
+        for i in range(999, -1, -1):
+            fls = t.lookup(records[i])
+            self.assertTrue(fls.fls[0].key == 0)
+            self.assertTrue(fls.fls[0].value == i)
+            self.assertTrue(fls.fls[1].key == 1)
+            self.assertTrue(fls.fls[1].value == "Hello %d" %i)
+        t.close()
+
+unittest.main()
+
